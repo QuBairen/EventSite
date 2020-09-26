706b9b02f7f9ba071acb70286e6485839ef43145
Michael Goins <bairen@expensivelittlehackbook.attlocal.net>
2020-09-26 14:20:04 -0500

    Update README with Reqs

--- eventsite/urls.py
+++ eventsite/urls.py
@@ -24,6 +24,5 @@ router.register(r'visitors',views.VisitorViewSet)
 router.register(r'registrations',views.RegistrationViewSet)
 
 urlpatterns = [
-    path('',include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/v1/',include(router.urls)),
 ]
